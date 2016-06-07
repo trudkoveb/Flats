1380a47f1dcb303817582b2831009c2d3a8d6d0e
Slava <lunatikspb@gmail.com>
2016-06-07 18:01:26 +0300

    new code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+  $("#owl").owlCarousel({
+    items: 3,
+    navigation : true,
+    navigationText : false,
+    rewindNav : true,
+    scrollPerPage : false,
+ });
+
+});
+
+
+
+
